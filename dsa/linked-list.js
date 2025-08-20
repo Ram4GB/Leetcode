e968999cb1a3520aed7fb5b4b1fd4eb3b6b920a9
Cuong Le <cuong.lm@cs.ventures>
2025-08-20 13:09:41 +0700

    init dsa folder

---
+++ dsa/linked-list.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+// 0,1,2,3,4,5
+// 0,x,1,2,3,4,5
+// add, remove, update
