a6242337e8da5a64fab297d3cd21c04da106ac59
Ram4GB <leminhcuong2988@yahoo.com.vn>
2025-08-21 08:28:22 +0700

    improve `move-zeroes.js`

--- move-zeroes.js
+++ move-zeroes.js
@@ -3,34 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  // two pointers
-  let i = 0;
-  let j;
-  let len = nums.length;
-  while (i < len) {
-    while (nums[i] !== 0 && i < len) {
-      i++;
+  let z=0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i]!==0) {
+      while(nums[z]!==0&&z<i) z++;
+      if (z!==i) {
+        nums[z] = nums[i];
+        nums[i] = 0;
+      }
     }
-    if (i === len - 1) break;
-    if (!j) j = i + 1;
-    while (nums[j] === 0 && j < len) {
-      j++;
-    }
-    if (j > len - 1) break;
-    const z = nums[i];
-    nums[i] = nums[j];
-    nums[j] = z;
-    i++;
-    j++;
   }
-
   return nums;
 };
 
-const t0 = performance.now();
-console.log("moveZeroes", moveZeroes([0, 1, 0, 3, 12])); // [ 1, 3, 12, 0, 0 ]
-const t1 = performance.now();
-console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
-
+// 0 0 1 0 3 0 12
+// console.log("moveZeroes", moveZeroes([0, 1, 0, 3, 12]));
+// i=0,j=1;
+// 1 0 0 3 12
+// i=1,j=3
+// 1 3 0 0 12
+// i=2,j=4
 console.log("moveZeroes", moveZeroes([0]));
 console.log("moveZeroes", moveZeroes([1]));
+console.log(moveZeroes([1,0]))
