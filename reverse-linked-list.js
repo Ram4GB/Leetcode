803a36c2716eb1f88976af77f104b9717d6929e9
Ram4GB <leminhcuong2988@yahoo.com.vn>
2025-08-23 19:17:27 +0700

    improve `reverse-linked-list`

--- reverse-linked-list.js
+++ reverse-linked-list.js
@@ -10,30 +10,20 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let node = head;
-    const arr = [];
-    while (node!==null) {
-      arr.push(node.val);
-      node = node.next;
-    }
-
-    // [1,2,3,4,5]
-
-    // build up the linked list from the array
-
-    let h = t = null;
-    for (let i = arr.length-1; i >=0; i--) {
-      const newNode = new ListNode(arr[i], null);
-      if (h === null) {
-        h = t = newNode;
-      } else {  
-        t.next = newNode;
-        t = newNode
-      }
-    }
-
-    return h;
+  if (!head) return null;
+    return revert(head, null);
 };
+
+const revert = (cur, prev) => {
+  const next = cur.next;
+  cur.next = prev;
+
+  if (next === null) {
+    return cur;
+  } else {
+    return revert(next, cur)
+  }
+}
 
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
@@ -53,3 +43,6 @@ while(n!==null) {
   console.log(n.val)
   n=n.next;
 }
+
+// cur=5, prev=null, next=4
+// 5->null
