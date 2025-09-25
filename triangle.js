daf9dc6bc78bd80213d37e3d23afb0a31416ed24
Ram4GB <leminhcuong2988@yahoo.com.vn>
2025-09-25 13:40:55 +0700

    resolve `./triangle.js`

--- triangle.js
+++ triangle.js
@@ -3,18 +3,29 @@
  * @return {number}
  */
 var minimumTotal = function (triangle) {
-  const cost = [triangle[0]];
-  for (let i = 1; i < triangle.length; i++) {
+  let dp = triangle[triangle.length - 1];
+  for (let i = triangle.length - 2; i >= 0; i--) {
     const arr = [];
-    arr[0] = triangle[i][0] + cost[i - 1][0];
-    arr[triangle[i].length - 1] = triangle[i][triangle[i].length - 1] + cost[i - 1][cost[i - 1].length - 1];
-    for (let j = 1; j < triangle[i].length - 1; j++) {
-      arr[j] = triangle[i][j] + Math.min(cost[i - 1][j], cost[i - 1][j - 1]);
+    for (let j = 0; j < triangle[i].length; j++) {
+      arr[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
     }
-    cost.push(arr);
+    dp = arr;
   }
-  return Math.min(...cost[cost.length - 1]);
+  return dp[0];
 };
+// var minimumTotal = function (triangle) {
+//   const cost = [triangle[0]];
+//   for (let i = 1; i < triangle.length; i++) {
+//     const arr = [];
+//     arr[0] = triangle[i][0] + cost[i - 1][0];
+//     arr[triangle[i].length - 1] = triangle[i][triangle[i].length - 1] + cost[i - 1][cost[i - 1].length - 1];
+//     for (let j = 1; j < triangle[i].length - 1; j++) {
+//       arr[j] = triangle[i][j] + Math.min(cost[i - 1][j], cost[i - 1][j - 1]);
+//     }
+//     cost.push(arr);
+//   }
+//   return Math.min(...cost[cost.length - 1]);
+// };
 
 console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])); // 11
 console.log(minimumTotal([[-10]]));
